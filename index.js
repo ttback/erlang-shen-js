109678e58b8ca8bc3897c2eaa1af91c4e9128e1c
Dev <ttback@gmail.com>
2014-05-18 01:11:52 -0400

    make it an usable commandline tool

--- index.js
+++ index.js
@@ -1,15 +1,85 @@
 #!/usr/bin/env node
+
+var program = require('commander');
 var fs = require('fs');
 var esprima = require('esprima');
 var exec = require('exec');
+var path = require('path');
+var mv = require('mv');
+var INPUT_ERLANG_FILE_PATH = undefined;
+var JS_AST_OUTPUT_ENABLED = false;
+var DEFAULT_JS_OUTPUT_DIR_PATH = "./";
+var JS_OUTPUT_FILE = undefined;
 
-exec(['erlc', '-pa', './deps', 'examples/sample.erl'], function(err, out, code) {
-  if (err instanceof Error)
-    throw err;
-  process.stderr.write(err);
- 
-  var jsCode = fs.readFileSync('./sample.js', "utf8");
-  console.log(JSON.stringify(esprima.parse(jsCode), null, 4));
+function ouptutAst() {
+    return JS_AST_OUTPUT_ENABLED = true;
+}
 
-  process.exit(code);
-});
+function outputFile(path) {
+    JS_OUTPUT_FILE = path;
+}
+
+function parseSourceCode(code) {
+
+}
+
+function deleteFiles(filename){
+	if (!JS_OUTPUT_FILE)
+		fs.unlinkSync(filename + ".js");
+	fs.unlinkSync(filename + ".beam");
+}
+
+program
+    .version('0.0.1')
+    .option('-t, --ast', 'Output js ast tree', ouptutAst)
+    .option('-o, --output <file>', 'Output to js file', outputFile)
+    .option('-c, --code <file>', 'erlang code', parseSourceCode)
+    .parse(process.argv);
+
+if (program.args.length > 0) {
+    INPUT_ERLANG_FILE_PATH = program.args[0];
+} else {
+    console.log("Please enter erlang file path. i.e. examples/samples.erl")
+    process.exit();
+}
+
+function cleanUp(code, filename){
+	deleteFiles(filename);
+
+	process.exit(code);
+}
+if (fs.existsSync(INPUT_ERLANG_FILE_PATH)) {
+    exec(['erlc', '-pa', './deps', INPUT_ERLANG_FILE_PATH], function(err, out, code) {
+        if (err instanceof Error)
+            throw err;
+        process.stderr.write(err);
+
+        var erlang_fileName = path.basename(INPUT_ERLANG_FILE_PATH);
+        var filename = erlang_fileName.split(".")[0];
+        var js_filename = filename + ".js";
+
+        var shen_output_js = DEFAULT_JS_OUTPUT_DIR_PATH + js_filename
+
+        var jsCode = fs.readFileSync(shen_output_js, "utf8");
+
+        var ast = esprima.parse(jsCode);
+        
+        if (JS_AST_OUTPUT_ENABLED) {
+            process.stdout.write(JSON.stringify(ast, null, 4));
+        } else {
+            process.stdout.write(jsCode);
+        }
+        
+        if (JS_OUTPUT_FILE) {
+			mv(shen_output_js, JS_OUTPUT_FILE, {mkdirp: true}, function(err) {
+				if (err instanceof Error)
+            		throw err;
+            	cleanUp(code, filename);
+			});
+        }else{
+        	cleanUp(code, filename);
+        }
+    });
+} else {
+    console.log("erlang file path is invalid");
+}
